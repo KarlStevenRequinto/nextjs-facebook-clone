71053f44ebe38a76b14cb4470d24b19cf3cd8a59
KarlStevenRequinto <lerdrobott@gmail.com>
2025-05-02 18:23:58 +0800

    like icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import MediaActionButton from "@/components/sub-components/media-action-button";
+import PostActionButton from "@/components/sub-components/post-action-button";
 import SideBarItem from "@/components/sub-components/sidebar-item";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
         <div className="flex items-center justify-center h-full gap-4 flex-col">
             <SideBarItem altText="test alt" imageSrc="/icons/facebook-icon.png" imageWidth={36} imageHeight={36} label="Lerd Robott" />
             <MediaActionButton altText="test alt" imageSrc="/icons/live-video-icon.png" imageWidth={24} imageHeight={24} label="Live video" />
+            <PostActionButton />
         </div>
     );
 }
---
+++ components/sub-components/post-action-button/index.tsx
@@ -0,0 +1,12 @@
+import LikeIcon from "@/public/generic-icons/like-icon";
+import React from "react";
+
+const PostActionButton = () => {
+    return (
+        <div>
+            <LikeIcon />
+        </div>
+    );
+};
+
+export default PostActionButton;
