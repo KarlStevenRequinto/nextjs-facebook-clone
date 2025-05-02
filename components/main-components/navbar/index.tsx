4a8c1fc54a4c31fc3923800e9394579b9ede94b3
KarlStevenRequinto <lerdrobott@gmail.com>
2025-05-02 17:55:07 +0800

    debugger, whats on your mind, partial nav bar

--- components/main-components/navbar/index.tsx
+++ components/main-components/navbar/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Navbar = () => {
-    return <nav className="h-[56px] bg-white sticky top-0 z-50 flex items-center px-4 shadow">Navbar</nav>;
+    return (
+        <nav className="h-[56px] bg-white sticky top-0 z-50 flex items-center justify-between px-4 shadow">
+            {/* Left - Icon + Searchbar */}
+            <div className="flex items-center gap-2">
+                <div>icon</div>
+                <div>searchbar</div>
+            </div>
+
+            {/* Center - Nav Buttons */}
+            <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-4">nav buttons</div>
+
+            {/* Right - Menus + Profile */}
+            <div className="flex items-center gap-2">
+                <div>menus</div>
+                <div>profile pic</div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
