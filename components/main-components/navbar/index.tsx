5dd78fab1e1595f9c156131e55c056b3785b2357
KarlStevenRequinto <lerdrobott@gmail.com>
2025-05-15 15:31:27 +0800

    search value bug fix

--- components/main-components/navbar/index.tsx
+++ components/main-components/navbar/index.tsx
@@ -21,6 +21,7 @@ const Navbar = () => {
     const [activeTab, setActiveTab] = useState("/");
     const [isSearchOpen, setIsSearchOpen] = useState(false);
     const [isSearchBarExpanded, setIsSearchBarExpanded] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
     const searchRef = useRef<HTMLInputElement | null>(null);
     const modalRef = useRef<HTMLDivElement | null>(null);
 
@@ -75,9 +76,14 @@ const Navbar = () => {
                         <input
                             type="text"
                             ref={searchRef}
+                            value={searchValue}
+                            onChange={(e) => setSearchValue(e.target.value)}
                             placeholder="Search Facebook"
                             onFocus={() => setIsSearchOpen(true)}
                             className="bg-transparent outline-none border-none placeholder:text-[color:var(--secondary-text)] placeholder:text-md pt-[7px] px-[8px] pb-[9px] w-[212px] h-full"
+                            style={{
+                                color: "var(--primary-black)",
+                            }}
                         />
                     </div>
 
@@ -144,14 +150,14 @@ const Navbar = () => {
 
             {isSearchOpen && (
                 <div
-                    className={`absolute left-0 top-0 w-[320px] h-[500px] bg-white rounded-lg shadow-xl pl-2 pr-4 py-2 z-100 transform transition-transform duration-300 ease-in-out ${
+                    className={`absolute left-0 top-0 w-[320px] h-[500px] bg-white rounded-lg shadow-xl pl-2 pr-4 py-2 z-100 transform transition-transform duration-500 ease-in-out ${
                         isSearchOpen ? "translate-x-0 opacity-100" : "-translate-x-[50%] opacity-0 pointer-events-none"
                     }`}
                     ref={modalRef}
                 >
                     <div className="relative flex items-center pl-2">
                         <div
-                            className={`w-[36px] h-[36px] rounded-full flex items-center justify-center hover:bg-[color:var(--comment-background)] cursor-pointer z-10 transition-transform duration-300 ease-in-out ${
+                            className={`w-[36px] h-[36px] rounded-full flex items-center justify-center hover:bg-[color:var(--comment-background)] cursor-pointer z-10 transition-transform duration-500 ease-in-out ${
                                 isSearchBarExpanded ? "-translate-x-2" : "translate-x-0"
                             }`}
                         >
@@ -172,10 +178,13 @@ const Navbar = () => {
                                 <input
                                     type="text"
                                     ref={searchRef}
+                                    value={searchValue}
+                                    onChange={(e) => setSearchValue(e.target.value)}
                                     placeholder="Search Facebook"
                                     onFocus={() => setIsSearchOpen(true)}
                                     className="bg-transparent outline-none border-none placeholder:text-[color:var(--secondary-text)] placeholder:text-md pt-[7px] pb-[9px] w-full h-full transition-[padding] duration-300"
                                     style={{
+                                        color: "var(--primary-black)",
                                         paddingLeft: isSearchBarExpanded ? "16px" : "8px",
                                         paddingRight: isSearchBarExpanded ? "16px" : "8px",
                                     }}
