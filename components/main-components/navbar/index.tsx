89cf76f5e5c5207457569ade478fef4705f98a4b
KarlStevenRequinto <lerdrobott@gmail.com>
2025-05-15 12:52:27 +0800

    icon update

--- components/main-components/navbar/index.tsx
+++ components/main-components/navbar/index.tsx
@@ -83,9 +83,7 @@ const Navbar = () => {
 
                     {/* Circle Search Icon: shown on <1280px */}
                     <div className="xl:hidden w-10 h-10 bg-[color:var(--comment-background)] rounded-full flex items-center justify-center">
-                        <svg className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                            <path d="M21 21l-4.35-4.35M10 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16z" />
-                        </svg>
+                        <SearchIcon fillColor="var(--secondary-text)" width="16" height="16" />
                     </div>
                 </div>
 
