e95f9e913d85a645f475edcc0cf1209537186dc9
KarlStevenRequinto <lerdrobott@gmail.com>
2025-05-19 21:28:43 +0800

    search icon animation

--- components/main-components/navbar/index.tsx
+++ components/main-components/navbar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, use } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import SearchIcon from "@/public/svg/search-icon";
@@ -22,10 +22,11 @@ const Navbar = () => {
     const [activeTab, setActiveTab] = useState("/");
     const [isSearchOpen, setIsSearchOpen] = useState(false);
     const [isSearchBarExpanded, setIsSearchBarExpanded] = useState(false);
+    const [isSearchFocused, setIsSearchFocused] = useState(false);
     const [searchValue, setSearchValue] = useState("");
     const searchRef = useRef<HTMLInputElement | null>(null);
     const modalRef = useRef<HTMLDivElement | null>(null);
-
+    const modalInputRef = useRef<HTMLInputElement | null>(null);
     const navItems = [
         { icon: <HomeIcon width="24" height="24" isActive={activeTab === "/"} />, name: "home", routeName: "/" },
         { icon: <VideoIcon width="24" height="24" isActive={activeTab === "watch"} />, name: "video", routeName: "watch" },
@@ -80,7 +81,12 @@ const Navbar = () => {
                             value={searchValue}
                             onChange={(e) => setSearchValue(e.target.value)}
                             placeholder="Search Facebook"
-                            onFocus={() => setIsSearchOpen(true)}
+                            onFocus={() => {
+                                setIsSearchOpen(true);
+                                setTimeout(() => {
+                                    modalInputRef.current?.focus();
+                                }, 100);
+                            }}
                             className="bg-transparent outline-none border-none placeholder:text-[color:var(--secondary-text)] placeholder:text-md pt-[7px] px-[8px] pb-[9px] w-[212px] h-full"
                             style={{
                                 color: "var(--primary-black)",
@@ -149,65 +155,72 @@ const Navbar = () => {
                 </div>
             </nav>
 
-            {isSearchOpen && (
-                <div
-                    className={` absolute left-0 top-0 w-[320px] h-[500px] bg-white rounded-lg shadow-xl px-2 py-2 z-100 transform transition-transform duration-500 ease-in-out ${
-                        isSearchOpen ? "translate-x-0 opacity-100" : "-translate-x-[50%] opacity-0 pointer-events-none"
-                    }`}
-                    ref={modalRef}
-                >
-                    <div className="relative flex items-center pl-2 ">
+            <div
+                className={` absolute left-0 top-0 w-[320px] h-[500px] bg-white rounded-lg shadow-xl px-2 py-2 z-100 transform transition-transform duration-500 ease-in-out overflow-hidden ${
+                    isSearchOpen ? "opacity-100 translate-y-0 pointer-events-auto scale-100" : "opacity-0 -translate-y-4 pointer-events-none scale-95"
+                }`}
+                ref={modalRef}
+            >
+                <div className="relative flex items-center pl-2 overflow-hidden">
+                    <div
+                        className={`w-[36px] h-[36px] rounded-full flex items-center justify-center hover:bg-[color:var(--comment-background)] cursor-pointer z-10 transition-transform duration-500 ease-in-out ${
+                            isSearchBarExpanded ? "-translate-x-2" : "translate-x-0"
+                        }`}
+                    >
+                        <ArrowLeftIcon width="20" height="20" fillColor="var(--secondary-text)" />
+                    </div>
+
+                    {/* Animated Search Bar */}
+                    <div className="relative w-[252px] h-[40px]">
                         <div
-                            className={`w-[36px] h-[36px] rounded-full flex items-center justify-center hover:bg-[color:var(--comment-background)] cursor-pointer z-10 transition-transform duration-500 ease-in-out ${
-                                isSearchBarExpanded ? "-translate-x-2" : "translate-x-0"
-                            }`}
+                            className={`absolute right-0 top-0 bg-[color:var(--comment-background)] text-white rounded-full h-[40px] transition-[width] duration-100 ease-in-out hidden xl:flex items-center`}
+                            style={{ width: isSearchBarExpanded ? "252px" : "240px" }}
                         >
-                            <ArrowLeftIcon width="20" height="20" fillColor="var(--secondary-text)" />
+                            {isSearchOpen && !isSearchFocused && (
+                                <span className="pl-3 absolute left-3 transition-opacity duration-200">
+                                    <SearchIcon fillColor="var(--secondary-text)" width="16" height="16" />
+                                </span>
+                            )}
+
+                            <input
+                                type="text"
+                                ref={modalInputRef}
+                                value={searchValue}
+                                onChange={(e) => setSearchValue(e.target.value)}
+                                placeholder="Search Facebook"
+                                onFocus={() => {
+                                    console.log("Input is focused");
+                                    setIsSearchOpen(true);
+                                    setIsSearchFocused(true);
+                                }}
+                                onBlur={() => {
+                                    console.log("Clicked outside input");
+                                    setIsSearchFocused(false);
+                                }}
+                                className="bg-transparent outline-none border-none placeholder:text-[color:var(--secondary-text)] placeholder:text-md pt-[7px] pb-[9px] w-full h-full transition-[padding] duration-300"
+                                style={{
+                                    color: "var(--primary-black)",
+                                    paddingLeft: isSearchBarExpanded ? "16px" : "8px",
+                                    paddingRight: isSearchBarExpanded ? "16px" : "8px",
+                                }}
+                            />
                         </div>
-
-                        {/* Animated Search Bar */}
-                        <div className="relative w-[252px] h-[40px]">
-                            <div
-                                className={`absolute right-0 top-0 bg-[color:var(--comment-background)] text-white rounded-full h-[40px] transition-[width] duration-100 ease-in-out hidden xl:flex items-center`}
-                                style={{ width: isSearchBarExpanded ? "252px" : "240px" }}
-                            >
-                                {!isSearchBarExpanded && (
-                                    <span className="pl-3">
-                                        <SearchIcon fillColor="var(--secondary-text)" width="16" height="16" />
-                                    </span>
-                                )}
-                                <input
-                                    type="text"
-                                    ref={searchRef}
-                                    value={searchValue}
-                                    onChange={(e) => setSearchValue(e.target.value)}
-                                    placeholder="Search Facebook"
-                                    onFocus={() => setIsSearchOpen(true)}
-                                    className="bg-transparent outline-none border-none placeholder:text-[color:var(--secondary-text)] placeholder:text-md pt-[7px] pb-[9px] w-full h-full transition-[padding] duration-300"
-                                    style={{
-                                        color: "var(--primary-black)",
-                                        paddingLeft: isSearchBarExpanded ? "16px" : "8px",
-                                        paddingRight: isSearchBarExpanded ? "16px" : "8px",
-                                    }}
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex items-center justify-between leading-[1.1765] mt-2 mb-[7px] px-2 pt-1.5">
-                        <h2 className="cursor-pointer text-[17px] font-semibold mb-1 relative top-[1.2px]">
-                            <span>Recent</span>
-                        </h2>
-
-                        <span className="text-[color:var(--blue-100)] cursor-pointer">Edit</span>
-                    </div>
-
-                    <div>
-                        <SearchItem />
-                        <SearchItem />
                     </div>
                 </div>
-            )}
+
+                <div className="flex items-center justify-between leading-[1.1765] mt-2 mb-[7px] px-2 pt-1.5">
+                    <h2 className="cursor-pointer text-[17px] font-semibold mb-1 relative top-[1.2px]">
+                        <span>Recent</span>
+                    </h2>
+
+                    <span className="text-[color:var(--blue-100)] cursor-pointer">Edit</span>
+                </div>
+
+                <div>
+                    <SearchItem />
+                    <SearchItem />
+                </div>
+            </div>
         </>
     );
 };
