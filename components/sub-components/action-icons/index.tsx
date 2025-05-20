56e692ac4e93d87f3fbe5cbfc7efabcaff28f768
KarlStevenRequinto <lerdrobott@gmail.com>
2025-05-20 19:34:03 +0800

    fix button active state bg colors

--- components/sub-components/action-icons/index.tsx
+++ components/sub-components/action-icons/index.tsx
@@ -10,26 +10,33 @@ interface ActionIconsProps {
 
 const ActionIcons = ({ icon, iconHeight, iconWidth, onClickIcon, isActive = false }: ActionIconsProps) => {
     const [isPressed, setIsPressed] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
+
+    const getBackgroundColor = () => {
+        if (isActive) {
+            if (isPressed) return "bg-[var(--blue-button-background-200)]";
+            if (isHovered) return "bg-[var(--blue-button-background-100)]";
+            return "bg-[var(--secondary-button-background)]";
+        } else {
+            if (isPressed) return "bg-[var(--background-100)]";
+            if (isHovered) return "bg-[var(--background-200)]";
+            return "bg-[var(--secondary-background)]";
+        }
+    };
+
     return (
         <div
             style={{ height: iconHeight, width: iconWidth }}
-            className={`flex items-center justify-center rounded-full
-                transition-all duration-100 cursor-pointer transform
-                ${
-                    isActive
-                        ? "bg-[var(--secondary-button-background)]"
-                        : isPressed
-                        ? "bg-[var(--background-100)]"
-                        : "hover:bg-[var(--background-200)] bg-[var(--secondary-background)]"
-                }
-                ${isPressed ? "scale-95" : "scale-100"}
-            `}
+            className={`flex items-center justify-center rounded-full transform transition-all duration-100 cursor-pointer ${getBackgroundColor()} ${
+                isPressed ? "scale-96" : "scale-100"
+            }`}
             onMouseDown={() => setIsPressed(true)}
             onMouseUp={() => setIsPressed(false)}
             onMouseLeave={() => setIsPressed(false)}
+            onMouseEnter={() => setIsHovered(true)}
             onClick={onClickIcon}
         >
-            <div className={`transform transition-transform duration-100 ${isPressed ? "scale-95" : "scale-100"}`}>{icon}</div>
+            <div className={`transform transition-transform duration-100 ${isPressed ? "scale-96" : "scale-100"}`}>{icon}</div>
         </div>
     );
 };
