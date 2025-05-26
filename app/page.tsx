303644cbbe260e324d4f256a5a29db7350546efa
KarlStevenRequinto <lerdrobott@gmail.com>
2025-05-26 17:27:32 +0800

    menu item component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import MediaActionButton from "@/components/sub-components/media-action-button";
+import MenuItem from "@/components/sub-components/menu-item";
 import PostActionButton from "@/components/sub-components/post-action-button";
 import SearchItem from "@/components/sub-components/search-item";
 import SideBarItem from "@/components/sub-components/sidebar-item";
@@ -10,6 +11,13 @@ export default function Home() {
             <MediaActionButton altText="test alt" imageSrc="/icons/live-video-icon.png" imageWidth={24} imageHeight={24} label="Live video" />
             <PostActionButton />
             <SearchItem />
+            <MenuItem
+                description="Create, manage and track the performance of your ads."
+                title="Ads Manager"
+                imgSource="/images/ads-manager.png"
+                imgWidth={36}
+                imgHeight={36}
+            />
         </div>
     );
 }
---
+++ components/sub-components/menu-item/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+
+interface MenuItemProps {
+    title: string;
+    description: string;
+    imgSource: string;
+    imgWidth?: number;
+    imgHeight?: number;
+}
+
+const MenuItem = ({ description, title, imgSource, imgWidth, imgHeight }: MenuItemProps) => {
+    return (
+        <div className="flex items-center bg-white rounded-lg p-2 hover:bg-[color:var(--comment-background)] transition-colors cursor-pointer w-[340px]">
+            <div className="px-1.5">
+                <Image width={imgWidth} height={imgHeight} alt={`${title} Icon`} src={imgSource} />
+            </div>
+            <div className="flex flex-col text-[color:var(--primary-black)] px-1.5">
+                <span className="font-semibold ">{title}</span>
+                <span className="text-[13px] leading-[18.166px]">{description}</span>
+            </div>
+        </div>
+    );
+};
+
+export default MenuItem;
